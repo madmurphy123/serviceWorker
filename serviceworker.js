475e36cc96dbab002da75ef8f1642b977a915405
MURPHY Aaron <m019189f@student.staffs.ac.uk>
2017-03-21 10:06:44 +0000

    first

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,63 @@
+
+var CACHE_NAME = 'gih-cache-v4';
+var CACHED_URLS = [
+  'offline.html',
+  'mystyles.css',
+  'dino.png'
+];
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith('gih-cache')) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //self.addEventListener('fetch', function(event) {
 //  console.log('Fetch request for:', event.request.url);
 //});
@@ -118,34 +178,7 @@
 //});
 
 
-var CACHE_NAME = 'gih-cache-v2';
-var CACHED_URLS = [
-  'offline.html',
-  'mystyles.css',
-  'dino.png'
-];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('offline.html');
-        }
-      });
-    })
-  );
-});
 
 
 
